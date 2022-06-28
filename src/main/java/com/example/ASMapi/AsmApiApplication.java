e69d2e0cdceef5d3591d8a430eecfdda7992e976
Yashar Mustafayev <yasarmustafayev1998@gmail.com>
2022-06-28 04:46:17 +0200

    initial import

---
+++ src/main/java/com/example/ASMapi/AsmApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.ASMapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsmApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsmApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ASMapi/AsmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ASMapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
